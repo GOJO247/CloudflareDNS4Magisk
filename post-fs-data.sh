0db7101189e4753edfee917eb6dd3dda8aada2ea
GOJO247 <138173607+GOJO247@users.noreply.github.com>
2025-03-02 19:14:54 -0500

    Add files via upload

---
+++ post-fs-data.sh
@@ -0,0 +1,7 @@
+#!/system/bin/sh
+resetprop -n net.dns1 1.1.1.1
+resetprop -n net.dns2 1.0.0.1
+resetprop -n net.ccmni1.dns1 1.1.1.1
+resetprop -n net.ccmni1.dns2 1.0.0.1
+resetprop -n net.dummy0.dns1 1.1.1.1
+resetprop -n net.dummy0.dns2 1.0.0.1
